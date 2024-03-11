06aed05de2e5aa3c8eaea7378e49898bb6bef6fa
Chirag Rathod <124911968+ChiragRathod25@users.noreply.github.com>
2024-03-11 19:26:08 +0530

    Update default.js

--- JS/default.js
+++ JS/default.js
@@ -1,4 +1,6 @@
+console.log('loaded');
 if(window.location.href.includes('chiragrathod25.github.io')){
+console.log('inside loaded');
   const addRootRapo=document.querySelectorAll('a')
 const repoName='/Sports-Inventory-Management';
 for(let i=0;i<addRootRapo.length;i++){
