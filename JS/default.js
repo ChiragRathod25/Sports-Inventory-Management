7e6a719539b4bb03e39ebc55f6ba1514136209cc
Chirag Rathod <124911968+ChiragRathod25@users.noreply.github.com>
2024-03-11 19:22:13 +0530

    Update default.js

--- JS/default.js
+++ JS/default.js
@@ -1,10 +1,9 @@
 if(window.location.href.includes('chiragrathod25.github.io')){
-const addRootRepo=document.querySelectorAll('a');
+  const addRootRapo=document.querySelectorAll('a')
 const repoName='/Sports-Inventory-Management';
-for(let i=0;i<addRootRepo.length; ++i){
-let val=addRootRepo[i].getAttribute('href');
-val = repoName+val;
-addRootRepo[i].setAttribute('href', val)
+for(let i=0;i<addRootRapo.length;i++){
+    let val=addRootRapo[i].getAttribute('href');
+    val=repoName+val;
+    addRootRapo[i].setAttribute('href',val)
 }
-
 }
