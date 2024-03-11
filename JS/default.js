664bf59b6ce76add9f02e116c5e9ef0f01d0d25f
Chirag Rathod <chiragrathod9987@gmail.com>
2024-03-11 09:59:27 +0530

    base

---
+++ JS/default.js
@@ -0,0 +1,5 @@
+if(window.location.href.includes('chiragrathod25.github.io')){
+    const base=document.createElement('base');
+    base.setAttribute('href','/Sports-Inventory-Management/');
+    document.querySelector('head').appendChild(base)
+}
