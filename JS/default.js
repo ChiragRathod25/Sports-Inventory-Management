98c4273f7adde63cf49473ad9b37b7d33efd81b5
Chirag Rathod <chiragrathod9987@gmail.com>
2024-03-11 19:46:34 +0530

    links#Update

--- JS/default.js
+++ JS/default.js
@@ -5,6 +5,9 @@ console.log('inside loaded');
 const repoName='/Sports-Inventory-Management';
 for(let i=0;i<addRootRapo.length;i++){
     let val=addRootRapo[i].getAttribute('href');
+    if(val==='#'){
+        continue;
+    }
     val=repoName+val;
     addRootRapo[i].setAttribute('href',val)
 }
