a08f49093dea5f05d0b4f03396b519096903b45e
Chirag Rathod <chiragrathod9987@gmail.com>
2024-04-07 15:09:18 +0530

    headerFooterJS

--- JS/default.js
+++ JS/default.js
@@ -1,13 +1,17 @@
 console.log("loaded");
-if (window.location.href.includes("chiragrathod25.github.io")) {
+if (window.location.href.includes("chiragrathod25.github.io")|| window.location.href.includes("localhost")) {
   console.log("inside loaded");
   const addRootRapo = document.querySelectorAll("a");
   const repoName = "/Sports-Inventory-Management";
-  for (let i = 0; i < addRootRapo.length; i++) {
+  for (let i = 0; i < addRootRapo.length; i++)  {
     let val = addRootRapo[i].getAttribute("href");
+    console.log('not changed')
     if (val != "" || val != "#") {
       val = repoName + val;
       addRootRapo[i].setAttribute("href", val);
+      console.log('changed');
     }
   }
+  console.log('after loop')
 }
+
