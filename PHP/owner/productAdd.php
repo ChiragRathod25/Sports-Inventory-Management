a4701fc18997d4c3603f9ed69a115d5d4a5098c3
Chirag Rathod <chiragrathod9987@gmail.com>
2024-04-19 21:49:07 +0530

    productAddColorSizeUpdate

--- PHP/owner/productAdd.php
+++ PHP/owner/productAdd.php
@@ -1,18 +1,17 @@
 <?php
-$server="localhost";
-$username="root";
-$password="Sports@Inv2937";
+$server = "localhost";
+$username = "root";
+$password = "Sports@Inv2937";
 $database = "Sports-Inventory-Management";
-$connect=mysqli_connect($server,$username,$password,$database);
-if(!$connect){
+$connect = mysqli_connect($server, $username, $password, $database);
+if (!$connect) {
     die("Connection failed: " . mysqli_connect_error());
 }
 require('checkuser.php');
 
-$sql="SELECT * FROM sport ORDER BY sport_id";
-$sports=mysqli_query($connect,$sql);
+$sql = "SELECT * FROM sport ORDER BY sport_id";
+$sports = mysqli_query($connect, $sql);
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -30,40 +29,24 @@ $sports=mysqli_query($connect,$sql);
     <my-header>
     </my-header>
     <section>
-     <form method="POST" id="productAddForm" onsubmit="return validateForm(this)" action="./validateProductAddForm.php">
+     <form method="POST" id="productAddForm" onsubmit="return validateForm(this)" action="./validateProductAddForm.php" enctype="multipart/form-data">
 
-            <label for="sport">Select Sport : </label>
-            <select name="sport" id="sport"   onchange="updateCategories(this.value)" > 
+            <label for="sport">Select Sport:</label>
+            <select name="sport" id="sport" onchange="updateCategories(this.value)"> 
                 <?php
-            $sql = "SELECT * FROM sport ORDER BY sport_id";
-            $result = mysqli_query($connect, $sql);
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<option value=\"" . $row["sport_id"] . "\"" . (isset($_POST['sport']) && $_POST['sport'] == $row["sport_id"] ? " selected" : "") . ">" . $row["name"] . "</option>";
-            }
-            ?>
-        </select>
+                while ($row = mysqli_fetch_assoc($sports)) {
+                    echo "<option value=\"" . $row["sport_id"] . "\"" . (isset($_POST['sport']) && $_POST['sport'] == $row["sport_id"] ? " selected" : "") . ">" . $row["name"] . "</option>";
+                }
+                ?>
+            </select>
         
         <a target="_blank" onclick='window.open("./sportAdd.php")'><button>Add Sports</button></a>
-            <label for="category">Select Category : </label>
-            <select name="category" id="category">
-                <?php
-            if (isset($_GET['sport_id'])) {
-                $sportId = $_GET['sport_id'];
-                $sql = "SELECT * FROM category WHERE sport_id = '$sportId' ORDER BY category_id";
-                $result = mysqli_query($connect, $sql);
-                if (mysqli_num_rows($result) > 0) {
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<option value=\"" . $row["category_id"] . "\">" . $row["name"] . "</option>";
-                    }
-                } else {
-                    echo "<option>No Category found</option>";
-                }
-            }
-            ?>
-        </select>
+        
+        <label for="category">Select Category:</label>
+        <select name="category" id="category"></select>
         <a target="_blank" onclick='window.open("./categoryAdd.php")'><button>Add Category</button></a> 
         
-        <label for="brand">Select Brand : </label>
+        <label for="brand">Select Brand:</label>
         <select name="brand" id="brand">
             <?php
             $sql = "SELECT * FROM brand ORDER BY brand_id";
@@ -74,6 +57,7 @@ $sports=mysqli_query($connect,$sql);
             ?>
         </select>
         <a target="_blank" onclick='window.open("./brandAdd.php")'><button>Add Brand</button></a> 
+        
         <label for="name">Product Name:</label>
         <input type="text" id="name" name="name">
             
@@ -83,26 +67,24 @@ $sports=mysqli_query($connect,$sql);
         <label for="description">Description:</label>
         <textarea id="description" name="description"></textarea>
        
-        <label for="image">Images of Product : </label>
+        <label for="image">Images of Product:</label>
         <input type="file" id="image" name="image[]" accept="image/*" multiple>
+        
+        <!-- Add fields for selecting sizes, colors, and quantities -->
+        <div id="variantsContainer" class="flex-column"></div>
+        <button type="button" id="addVariant">Add Variant</button>
         
         <div id="specificationsContainer" class="flex-column"></div>
         <button type="button" id="addSpecification">Add Specification</button>
             
         <button type="submit" id="submit">Submit</button>
-
     </form>
     </section>
     <my-footer>
     </my-footer>
-    
 </body>
 
 <script src="../../JS/headerFooter.js"></script>
-  <script src="../../JS/default.js"></script>
-  
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
-
-  <script src="../../JS/Team Sports/productAdd.js"></script>
-
+<script src="../../JS/default.js"></script>
+<script src="../../JS/Team Sports/productAdd.js"></script>
 </html>
