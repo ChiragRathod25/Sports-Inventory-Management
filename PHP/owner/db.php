35a567afcdde0cfdde8ae5072c62634a999a2032
Chirag Rathod <chiragrathod9987@gmail.com>
2024-06-19 17:52:44 +0530

    merge fixed

--- PHP/owner/db.php
+++ PHP/owner/db.php
@@ -1,19 +1,5 @@
 <?php
-<<<<<<< HEAD
-$server="localhost";
-$username="jxeymueq_Sports-Inventory-Management";
-$password="a6]1yM5:t3wQXF";
-$database = "jxeymueq_Sports-Inventory-Management";
-
-$connect=mysqli_connect($server,$username,$password,$database);
-$conn=$connect;
-$con=$connect;
-if(!$connect){
-    die("Connection failed: " . mysqli_connect_error());
-}
-=======
 require('../database_connection.php');
 $conn=$connect;
 $con=$connect;
->>>>>>> 8445a1f (delete-add-validation fix)
 ?>
